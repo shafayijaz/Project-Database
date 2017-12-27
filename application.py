0c214d056194f20e9abd81aa09a05b231db74181
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2017-12-27 16:14:25 +0500

    'About' and the 'Course' Tab

--- application.py
+++ application.py
@@ -9,6 +9,14 @@ db = SQL("sqlite:///DB-Proj.db")
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contactUs")
+def contactUs():
+    return render_template("contactUs.html")
+
 # Gives options for update, view & Delete
 @app.route("/school")
 def school():
@@ -34,8 +42,8 @@ def InsSchool():
 def schoolInsert():
     if request.form["Sname"] == "" or request.form["Location"] == "":
         return render_template("failure.html")
-    db.execute("INSERT INTO School(Sname, Location, PhoneNo, Website) VALUES(:Sname, :Location, :PhoneNo, :Website)",
-        Sname=request.form["Sname"], Location=request.form["Location"], PhoneNo=request.form["PhoneNo"], Website=request.form["Website"])
+    db.execute("INSERT INTO School(RegNo, Sname, Location, PhoneNo, Website) VALUES(:RegNo, :Sname, :Location, :PhoneNo, :Website)",
+        RegNo=request.form["RegNo"], Sname=request.form["Sname"], Location=request.form["Location"], PhoneNo=request.form["PhoneNo"], Website=request.form["Website"])
     return render_template("success.html")
 
 # deals with View of the table
@@ -54,3 +62,51 @@ def schoolDelete():
         if request.form["RegNo"]:
             db.execute("DELETE FROM School WHERE RegNo = :RegNo",RegNo=request.form["RegNo"])
         return redirect(url_for("schoolView"))
+
+# COURSE
+# Gives options for update, view & Delete
+@app.route("/course")
+def course():
+    return render_template("courses/Course.html")
+
+# Handles the url
+@app.route("/handleCourse", methods=["POST", "GET"])
+def handleCourse():
+    if request.form["button"] == "courseView":
+        return redirect(url_for("courseView"))
+    elif request.form["button"] == "courseDelete":
+        return redirect(url_for("courseDelete"))
+    elif request.form["button"] == "InsCourse":
+        return redirect(url_for("InsCourse"))
+
+# renders a new html file
+@app.route("/InsCourse")
+def InsCourse():
+    rows = db.execute("SELECT SchRegNo FROM Course")
+    return render_template("courses/InsCourse.html", InsCourse = rows)
+
+# deals with updation of the table
+@app.route("/courseInsert", methods=["POST"])
+def courseInsert():
+    if request.form["CourseId"] == "" or request.form["Cname"] == "":
+        return render_template("failure.html")
+    db.execute("INSERT INTO Course(CourseId, Cname, Level, SchRegNo) VALUES(:CourseId, :Cname, :Level, :SchRegNo)",
+        CourseId=request.form["CourseId"], Cname=request.form["Cname"], Level=request.form["Level"], SchRegNo=request.form["SchRegNo"])
+    return render_template("success.html")
+
+# deals with View of the table
+@app.route("/courseView")
+def courseView():
+    rows = db.execute("SELECT * FROM Course")
+    return render_template("courses/courseView.html", courseView = rows)
+
+# deals with Deletion of the table
+@app.route("/courseDelete", methods= ["GET", "POST"])
+def courseDelete():
+    if request.method == "GET":
+        rows = db.execute("SELECT * FROM Course")
+        return render_template("courses/courseDelete.html", courseDelete= rows)
+    elif request.method == "POST":
+        if request.form["CourseId"]:
+            db.execute("DELETE FROM Course WHERE CourseId = :CourseId",CourseId=request.form["CourseId"])
+        return redirect(url_for("courseView"))
